e6b25388dbfbfaee6a27b0a064fe6b92eaa948b2
Maksym Shpakovych <maksym.shpakovych@gmail.com>
2022-01-30 14:53:53 +0100

    File was reformatted.

--- methods.py
+++ methods.py
@@ -4,15 +4,15 @@ import numpy as np
 from loop import loop
 
 
-def alternating_projection(tm, b, *,
-                           x0: np.array = None,
-                           tol: float = typedef.DEFAULT_TOL,
-                           max_iter: int = typedef.DEFAULT_MAX_ITER,
-                           metric: callable = typedef.DEFAULT_METRIC,
-                           callbacks: typing.List[callable] = None,
-                           decorators: typing.List[callable] = None,
-                           seed: int = None,
-                           tm_pinv: np.ndarray = None):
+def alternating_projections(tm, b, *,
+                            x0: np.array = None,
+                            tol: float = typedef.DEFAULT_TOL,
+                            max_iter: int = typedef.DEFAULT_MAX_ITER,
+                            metric: callable = typedef.DEFAULT_METRIC,
+                            callbacks: typing.List[callable] = None,
+                            decorators: typing.List[callable] = None,
+                            seed: int = None,
+                            tm_pinv: np.ndarray = None):
     if x0 is None:
         x0 = np.shape(tm)[1]
     if tm_pinv is None:
@@ -20,4 +20,5 @@ def alternating_projection(tm, b, *,
 
     def update(x):
         return tm_pinv.dot(b * np.exp(1j * np.angle(tm.dot(x))))
+
     return loop(update, x0, tol, max_iter, metric, callbacks, decorators, seed)
