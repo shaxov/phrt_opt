ae4766e306183fde300129456e9031f2aef1cc6e
Maksym Shpakovych <maksym.shpakovych@gmail.com>
2022-01-30 17:15:05 +0100

    Operations counter decorator was implemented.

---
+++ phrt_opt/decorators.py
@@ -0,0 +1,14 @@
+
+
+def ops_count(count):
+    def _update_wrap(update):
+        def _update(*args, **kwargs):
+            x = update(*args, **kwargs)
+            dinfo = []
+            if isinstance(x, tuple):
+                x, dinfo = x
+            sub_ops_cnt = kwargs.get('sub_ops', [0]).pop()
+            dinfo.append(count + sub_ops_cnt)
+            return x, dinfo
+        return _update
+    return _update_wrap
