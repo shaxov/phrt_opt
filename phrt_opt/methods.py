edc247d8ba5a9ce2d51024ce7bef449841cd46c2
Maksym Shpakovych <maksym.shpakovych@gmail.com>
2022-01-30 19:41:37 +0100

    Dual ascent method was implemented.

--- phrt_opt/methods.py
+++ phrt_opt/methods.py
@@ -55,3 +55,30 @@ def phare_admm(tm, b, *,
         return x
 
     return loop(update, x0, tol, max_iter, metric, callbacks, decorators, seed)
+
+
+def dual_ascent(tm, b, *,
+                x0: np.array = None,
+                tol: float = typedef.DEFAULT_TOL,
+                max_iter: int = typedef.DEFAULT_MAX_ITER,
+                metric: callable = typedef.DEFAULT_METRIC,
+                callbacks: typing.List[callable] = None,
+                decorators: typing.List[callable] = None,
+                seed: int = None,
+                tm_pinv: np.ndarray = None):
+    if x0 is None:
+        x0 = np.shape(tm)[1]
+    if tm_pinv is None:
+        tm_pinv = np.linalg.pinv(tm)
+    m = np.shape(tm)[0]
+    lmd = np.zeros(shape=(m, 1)) + 1j * np.zeros(shape=(m, 1))
+
+    def update(x):
+        nonlocal lmd
+        tm_x = tm.dot(x)
+        b_exp_tht = b * np.exp(1j * np.angle(tm_x + lmd))
+        x = tm_pinv.dot(b_exp_tht)
+        lmd = lmd + tm.dot(x) - b_exp_tht
+        return x
+
+    return loop(update, x0, tol, max_iter, metric, callbacks, decorators, seed)
