dea45247be6fbabf7994f72d590e736fb1d69379
Maksym Shpakovych <maksym.shpakovych@gmail.com>
2022-01-30 19:54:11 +0100

    Relaxed dual ascent method was implemented.

--- phrt_opt/methods.py
+++ phrt_opt/methods.py
@@ -1,5 +1,6 @@
 import typing
 import numpy as np
+from phrt_opt import metrics
 from phrt_opt import typedef
 from phrt_opt.loop import loop
 
@@ -79,6 +80,37 @@ def dual_ascent(tm, b, *,
         b_exp_tht = b * np.exp(1j * np.angle(tm_x + lmd))
         x = tm_pinv.dot(b_exp_tht)
         lmd = lmd + tm.dot(x) - b_exp_tht
+        return x
+
+    return loop(update, x0, tol, max_iter, metric, callbacks, decorators, seed)
+
+
+def relaxed_dual_ascent(tm, b, *,
+                        x0: np.array = None,
+                        tol: float = typedef.DEFAULT_TOL,
+                        max_iter: int = typedef.DEFAULT_MAX_ITER,
+                        metric: callable = typedef.DEFAULT_METRIC,
+                        callbacks: typing.List[callable] = None,
+                        decorators: typing.List[callable] = None,
+                        seed: int = None,
+                        tm_pinv: np.ndarray = None,
+                        rho: float = .5):
+    if x0 is None:
+        x0 = np.shape(tm)[1]
+    if tm_pinv is None:
+        tm_pinv = np.linalg.pinv(tm)
+    m = np.shape(tm)[0]
+    lmd = np.zeros(shape=(m, 1)) + 1j * np.zeros(shape=(m, 1))
+    eps = np.zeros(shape=(m, 1)) + 1j * np.zeros(shape=(m, 1))
+
+    def update(x):
+        nonlocal lmd, eps
+        tm_x = tm.dot(x)
+        z = b * np.exp(1j * np.angle(tm_x - eps + lmd))
+        x = tm_pinv.dot(z + eps - lmd)
+        y = tm.dot(x)
+        eps = (rho / (1 + rho)) * (y - z + lmd)
+        lmd = lmd + y - z - eps
         return x
 
     return loop(update, x0, tol, max_iter, metric, callbacks, decorators, seed)
