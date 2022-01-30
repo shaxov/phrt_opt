267c338467c1b98b79253f0d7693fd867638edc8
Maksym Shpakovych <maksym.shpakovych@gmail.com>
2022-01-30 19:37:07 +0100

    Ops counter for Phare ADMM was implemented.

--- phrt_opt/ops/counters.py
+++ phrt_opt/ops/counters.py
@@ -12,3 +12,16 @@ def alternating_projections(m, n):
     ops_cnt += 2 * m * r_ops_costs.prod        # z = b * e_phi
     ops_cnt += n * c_ops_costs.vdot(m)         # x = A_pinv * z
     return ops_cnt
+
+
+def phare_admm(m, n):
+    ops_cnt = m * c_ops_costs.vdot(n)                                                               # Ax = A*x = [a1Tx, ..., amTx]
+    ops_cnt += 2 * m * r_ops_costs.div + m * c_ops_costs.add                                        # g = Ax - lmd / rho
+    ops_cnt += m * c_ops_costs.angle                                                                # tht = angle(g)
+    ops_cnt += m * (c_ops_costs.abs + 2 * r_ops_costs.prod + r_ops_costs.add + r_ops_costs.div)     # u = (rho * abs(g) + b) / (rho + 1)
+    ops_cnt += m * (c_ops_costs.exp + 2 * r_ops_costs.prod)                                         # u_exp_tht = u * exp(1j * tht)
+    ops_cnt += m * c_ops_costs.sub                                                                  # y = u_exp_tht - lmd / rho
+    ops_cnt += n * c_ops_costs.vdot(m)                                                              # x = A_pinv*y
+    ops_cnt += m * c_ops_costs.sub                                                                  # reg = Ax - u_exp_tht
+    ops_cnt += m * 2 * (r_ops_costs.prod + r_ops_costs.add)                                         # lmd = lmd + rho * reg
+    return ops_cnt
