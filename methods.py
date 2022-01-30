669a2b74f2f5df3d3accf3eedf0a79bd07c371c0
Maksym Shpakovych <maksym.shpakovych@gmail.com>
2022-01-30 14:51:41 +0100

    Alternating projection method was implemented.

--- methods.py
+++ methods.py
@@ -1,16 +1,23 @@
 import typing
 import typedef
 import numpy as np
+from loop import loop
 
 
-def _loop(
-        update: callable,
-        x0: typing.Union[np.array, int] = None,
-        tol: float = typedef.DEFAULT_TOL,
-        max_iter: int = typedef.DEFAULT_MAX_ITER,
-        metric: callable = typedef.DEFAULT_METRIC,
-        callbacks: typing.List[callable] = None,
-        decorators: typing.List[callable] = None,
-        seed: int = None,
-):
-    pass
+def alternating_projection(tm, b, *,
+                           x0: np.array = None,
+                           tol: float = typedef.DEFAULT_TOL,
+                           max_iter: int = typedef.DEFAULT_MAX_ITER,
+                           metric: callable = typedef.DEFAULT_METRIC,
+                           callbacks: typing.List[callable] = None,
+                           decorators: typing.List[callable] = None,
+                           seed: int = None,
+                           tm_pinv: np.ndarray = None):
+    if x0 is None:
+        x0 = np.shape(tm)[1]
+    if tm_pinv is None:
+        tm_pinv = np.linalg.pinv(tm)
+
+    def update(x):
+        return tm_pinv.dot(b * np.exp(1j * np.angle(tm.dot(x))))
+    return loop(update, x0, tol, max_iter, metric, callbacks, decorators, seed)
