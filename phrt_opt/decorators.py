8f49af6e397ab211e6667b0c48c5fffe99e30723
Maksym Shpakovych <maksym.shpakovych@gmail.com>
2022-01-31 22:46:36 +0100

    Ops count decorator was refactored.

--- phrt_opt/decorators.py
+++ phrt_opt/decorators.py
@@ -1,12 +1,20 @@
+from phrt_opt import typedef
+
+
 def ops_count(count):
-    def _update_wrap(update):
-        def _update(*args, **kwargs):
-            x = update(*args, **kwargs)
+    def _func_wrap(func):
+        def _func(*args, **kwargs):
+            try:
+                kwargs.update({typedef.DECORATOR_OPS_COUNT_NAME: [0]})
+                x = func(*args, **kwargs)
+            except TypeError:
+                kwargs.pop(typedef.DECORATOR_OPS_COUNT_NAME)
+                x = func(*args, **kwargs)
             dinfo = []
             if isinstance(x, tuple):
                 x, dinfo = x
-            sub_ops_cnt = kwargs.get('sub_ops', [0]).pop()
+            sub_ops_cnt = kwargs.get(typedef.DECORATOR_OPS_COUNT_NAME, [0]).pop()
             dinfo.append(count + sub_ops_cnt)
             return x, dinfo
-        return _update
-    return _update_wrap
+        return _func
+    return _func_wrap
