3c0b81c7d7b886431055c9b85d08813ac2ec384e
Maksym Shpakovych <maksym.shpakovych@gmail.com>
2022-05-02 15:26:37 +0200

    Tests were fixed.

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -13,6 +13,7 @@ class TestDecorators(unittest.TestCase):
         self.num_beams = n
         self.num_measurements = m
         self.x = np.exp(1j * random.uniform(-np.pi, np.pi, size=(n, 1)))
+        self.x0 = np.exp(1j * random.uniform(-np.pi, np.pi, size=(n, 1)))
         self.tm = random.randn(m, n) + 1j * random.randn(m, n)
         self.b = np.abs(self.tm.dot(self.x))
 
@@ -20,69 +21,73 @@ class TestDecorators(unittest.TestCase):
         from phrt_opt.decorators import ops_count
         from phrt_opt.ops.counters import alternating_projections
 
-        x = self.x
+        x, x0 = self.x, self.x0
         n = self.num_beams
         m = self.num_measurements
         x_bar, info = phrt_opt.methods.alternating_projections(
             self.tm, self.b,
+            x0=x0,
             tol=1e-6,
             max_iter=100,
             seed=self.seed,
             decorators=[ops_count(alternating_projections(m, n))],
         )
         dist = phrt_opt.metrics.quality_norm(x, x_bar)
-        self.assertAlmostEqual(dist, 8.42524891297991e-06)
+        self.assertAlmostEqual(dist, 0.00024642286613196784)
 
     def test_phare_admm_ops_count_decorator(self):
         from phrt_opt.decorators import ops_count
         from phrt_opt.ops.counters import phare_admm
 
-        x = self.x
+        x, x0 = self.x, self.x0
         n = self.num_beams
         m = self.num_measurements
         x_bar, info = phrt_opt.methods.phare_admm(
             self.tm, self.b,
+            x0=x0,
             tol=1e-6,
             max_iter=100,
             seed=self.seed,
             decorators=[ops_count(phare_admm(m, n))],
         )
         dist = phrt_opt.metrics.quality_norm(x, x_bar)
-        self.assertAlmostEqual(dist, 1.2015331742665225e-05)
+        self.assertAlmostEqual(dist, 2.682048674373938e-05)
 
     def test_dual_ascent_ops_count_decorator(self):
         from phrt_opt.decorators import ops_count
         from phrt_opt.ops.counters import dual_ascent
 
-        x = self.x
+        x, x0 = self.x, self.x0
         n = self.num_beams
         m = self.num_measurements
         x_bar, info = phrt_opt.methods.dual_ascent(
             self.tm, self.b,
+            x0=x0,
             tol=1e-6,
             max_iter=100,
             seed=self.seed,
             decorators=[ops_count(dual_ascent(m, n))],
         )
         dist = phrt_opt.metrics.quality_norm(x, x_bar)
-        self.assertAlmostEqual(dist, 2.4150490046559447e-05)
+        self.assertAlmostEqual(dist, 0.0004532004668830192)
 
     def test_relaxed_dual_ascent_ops_count_decorator(self):
         from phrt_opt.decorators import ops_count
         from phrt_opt.ops.counters import relaxed_dual_ascent
 
-        x = self.x
+        x, x0 = self.x, self.x0
         n = self.num_beams
         m = self.num_measurements
         x_bar, info = phrt_opt.methods.relaxed_dual_ascent(
             self.tm, self.b,
+            x0=x0,
             tol=1e-6,
             max_iter=100,
             seed=self.seed,
             decorators=[ops_count(relaxed_dual_ascent(m, n))],
         )
         dist = phrt_opt.metrics.quality_norm(x, x_bar)
-        self.assertAlmostEqual(dist, 3.2475099216444647e-06)
+        self.assertAlmostEqual(dist, 2.2636871992776086e-05)
 
     def test_wirtinger_ops_count_decorator(self):
         from phrt_opt import decorators
@@ -98,5 +103,5 @@ class TestDecorators(unittest.TestCase):
         decorator = decorators.ops_count(counters.wirtinger(m, n))
         initializer = decorator(initializer)
         x0, dinfo = initializer(tm, b)
-        self.assertAlmostEqual(dinfo[0], 1.60256852e-05)
+        self.assertAlmostEqual(dinfo[0], 1.5355108091000000868e-05)
 
--- tests/test_methods.py
+++ tests/test_methods.py
@@ -13,60 +13,66 @@ class TestMethods(unittest.TestCase):
         self.num_beams = n
         self.num_measurements = m
         self.x = np.exp(1j * random.uniform(-np.pi, np.pi, size=(n, 1)))
+        self.x0 = np.exp(1j * random.uniform(-np.pi, np.pi, size=(n, 1)))
         self.tm = random.randn(m, n) + 1j * random.randn(m, n)
         self.b = np.abs(self.tm.dot(self.x))
 
     def test_alternating_projections(self):
-        x = self.x
+        x, x0 = self.x, self.x0
         x_bar = phrt_opt.methods.alternating_projections(
             self.tm, self.b,
+            x0=x0,
             tol=1e-6,
             max_iter=100,
             seed=self.seed,
         )
         dist = phrt_opt.metrics.quality_norm(x, x_bar)
-        self.assertAlmostEqual(dist, 8.42524891297991e-06)
+        self.assertAlmostEqual(dist, 0.00024642286613196784)
 
     def test_phare_admm(self):
-        x = self.x
+        x, x0 = self.x, self.x0
         x_bar = phrt_opt.methods.phare_admm(
             self.tm, self.b,
+            x0=x0,
             tol=1e-6,
             max_iter=100,
             seed=self.seed,
         )
         dist = phrt_opt.metrics.quality_norm(x, x_bar)
-        self.assertAlmostEqual(dist, 1.2015331742665225e-05)
+        self.assertAlmostEqual(dist, 2.682048674373938e-05)
 
     def test_dual_ascent(self):
-        x = self.x
+        x, x0 = self.x, self.x0
         x_bar = phrt_opt.methods.dual_ascent(
             self.tm, self.b,
+            x0=x0,
             tol=1e-6,
             max_iter=100,
             seed=self.seed,
         )
         dist = phrt_opt.metrics.quality_norm(x, x_bar)
-        self.assertAlmostEqual(dist, 2.4150490046559447e-05)
+        self.assertAlmostEqual(dist, 0.0004532004668830192)
 
     def test_relaxed_dual_ascent(self):
-        x = self.x
+        x, x0 = self.x, self.x0
         x_bar = phrt_opt.methods.relaxed_dual_ascent(
             self.tm, self.b,
+            x0=x0,
             tol=1e-6,
             max_iter=100,
             seed=self.seed,
         )
         dist = phrt_opt.metrics.quality_norm(x, x_bar)
-        self.assertAlmostEqual(dist, 3.2475099216444647e-06)
+        self.assertAlmostEqual(dist, 2.2636871992776086e-05)
 
     def test_accelerated_relaxed_dual_ascent(self):
-        x = self.x
+        x, x0 = self.x, self.x0
         x_bar = phrt_opt.methods.accelerated_relaxed_dual_ascent(
             self.tm, self.b,
+            x0=x0,
             tol=1e-6,
             max_iter=100,
             seed=self.seed,
         )
         dist = phrt_opt.metrics.quality_norm(x, x_bar)
-        self.assertAlmostEqual(dist, 0.0002727315687515164)
+        self.assertAlmostEqual(dist, 0.00143715897585861)
