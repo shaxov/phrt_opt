14e4622800e3dd7674e551cf7a85f21e76143aff
Maksym Shpakovych <maksym.shpakovych@gmail.com>
2022-05-06 13:05:50 +0200

    Typedef structure was refactored.

--- phrt_opt/typedef.py
+++ phrt_opt/typedef.py
@@ -1,22 +1,23 @@
-from phrt_opt import metrics
-from phrt_opt import quadprog
-from phrt_opt import linesearch
-from phrt_opt import strategies
-
 DEFAULT_TOL = 1e-8
+DEFAULT_REG_DLT = 1e-8
 DEFAULT_MAX_ITER = 1000
+DEFAULT_POWER_METHOD_TOL = 1e-3
+
+from phrt_opt import metrics
 DEFAULT_METRIC = metrics.quality_norm
+
+from phrt_opt import strategies
 DEFAULT_RHO_STRATEGY = strategies.constant_strategy()
-DECORATOR_INFO_KEY = "dinfo"
-CALLBACK_INFO_KEY = "cinfo"
-DECORATOR_OPS_COUNT_NAME = "_ops_count"
 
 DEFAULT_BACKTRACKING_RATE = .5
 DEFAULT_BACKTRACKING_MAX_ITER = 100
 DEFAULT_BACKTRACKING_ALPHA0 = 1.
 DEFAULT_BACKTRACKING_C1 = 1e-4
-
-DEFAULT_POWER_METHOD_TOLERANCE = 1e-3
-
-DEFAULT_QUADPROG_SOLVER = quadprog.conjugate_gradient_solver
+from phrt_opt import linesearch
 DEFAULT_LINESEARCH = linesearch.backtracking
+
+DEFAULT_CG_TOL = 1e-8
+DEFAULT_CG_MAX_ITER = None
+
+from phrt_opt import quadprog
+DEFAULT_QUADPROG_SOLVER = quadprog.conjugate_gradient_solver
