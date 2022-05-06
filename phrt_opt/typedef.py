6369a211bc7e916f27bc55a6bccbf0e8fd77b3bf
Maksym Shpakovych <maksym.shpakovych@gmail.com>
2022-05-06 10:30:54 +0200

    Strategy was renamed to rho_strategy.

--- phrt_opt/typedef.py
+++ phrt_opt/typedef.py
@@ -6,7 +6,7 @@ from phrt_opt import strategies
 DEFAULT_TOL = 1e-8
 DEFAULT_MAX_ITER = 1000
 DEFAULT_METRIC = metrics.quality_norm
-DEFAULT_STRATEGY = strategies.constant_strategy()
+DEFAULT_RHO_STRATEGY = strategies.constant_strategy()
 DECORATOR_INFO_KEY = "dinfo"
 CALLBACK_INFO_KEY = "cinfo"
 DECORATOR_OPS_COUNT_NAME = "_ops_count"
@@ -19,4 +19,4 @@ DEFAULT_BACKTRACKING_C1 = 1e-4
 DEFAULT_POWER_METHOD_TOLERANCE = 1e-3
 
 DEFAULT_QUADPROG_SOLVER = quadprog.conjugate_gradient_solver
-DEFAULT_LINESEARCH_METHOD = linesearch.backtracking
+DEFAULT_LINESEARCH = linesearch.backtracking
