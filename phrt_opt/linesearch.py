7bd27faed5a5a3c6df639a015d712114d3c33f1a
Maksym Shpakovych <maksym.shpakovych@gmail.com>
2022-05-06 09:32:16 +0200

    Comments to linesearch were added.

--- phrt_opt/linesearch.py
+++ phrt_opt/linesearch.py
@@ -1,15 +1,21 @@
+import typedef
 import numpy as np
 
 
-def backtracking(fun, x, gk, c1=1e-4, rate=0.5, max_iter=100, alpha0=1.):
-    """ Armijo backtracking line search. [1, p.33, (3.4)]
+def backtracking(
+        fun, x, gk,
+        c1=typedef.DEFAULT_BACKTRACKING_C1,
+        rate=typedef.DEFAULT_BACKTRACKING_RATE,
+        max_iter=typedef.DEFAULT_BACKTRACKING_MAX_ITER,
+        alpha0=typedef.DEFAULT_BACKTRACKING_ALPHA0):
+    """ Armijo backtracking line search [1, p.33, (3.4)].
 
     Reference:
         [1] Jorge Nocedal, Stephen J. Wright, Numerical Optimization, 2nd edition.
     """
     fun_x = fun(x)
     alpha = alpha0
-    gk_norm2 = np.linalg.norm(gk) ** 2
+    gk_norm2 = np.linalg.norm(gk)**2
     for _ in range(max_iter):
         h = fun(x - alpha * gk) + alpha * c1 * gk_norm2
         if fun_x > h: break
@@ -18,24 +24,46 @@ def backtracking(fun, x, gk, c1=1e-4, rate=0.5, max_iter=100, alpha0=1.):
 
 
 class secant:
+    """ Secant equation based line search [1].
+
+    Reference:
+        [1] JONATHAN BARZILAI, JONATHAN M. BORWEIN, Two-Point Step Size Gradient Methods,
+            IMA Journal of Numerical Analysis, Volume 8, Issue 1, January 1988, Pages 141–148,
+    """
 
     def __init__(self):
         self.prev_x, self.prev_gk = None, None
 
-    def __call__(self, fun, x, gk, c1=1e-4, rate=0.5, max_iter=100, alpha0=1.):
+    def __call__(self,
+                 fun, x, gk,
+                 c1=typedef.DEFAULT_BACKTRACKING_C1,
+                 rate=typedef.DEFAULT_BACKTRACKING_RATE,
+                 max_iter=typedef.DEFAULT_BACKTRACKING_MAX_ITER,
+                 alpha0=typedef.DEFAULT_BACKTRACKING_ALPHA0):
         if self.prev_x is None or self.prev_gk is None:
             self.prev_x, self.prev_gk = x, gk
-            return 1.
+            return backtracking(fun, x, gk, c1, rate, max_iter, alpha0)
         sk, yk = x - self.prev_x, gk - self.prev_gk
         self.prev_x, self.prev_gk = x, gk
         ck = np.real(np.vdot(sk, yk))
         if ck > 0: return np.vdot(sk, sk) / ck
-        return 1.
+        return backtracking(fun, x, gk, c1, rate, max_iter, alpha0)
 
 
 class secant_symmetric(secant):
+    """ Secant equation based line search (symmetric variant) [1].
 
-    def __call__(self, fun, x, gk, c1=1e-4, rate=0.5, max_iter=5, alpha0=1.):
+    Reference:
+        [1] JONATHAN BARZILAI, JONATHAN M. BORWEIN, Two-Point Step Size Gradient Methods,
+            IMA Journal of Numerical Analysis, Volume 8, Issue 1, January 1988, Pages 141–148,
+    """
+
+    def __call__(self,
+                 fun, x, gk,
+                 c1=typedef.DEFAULT_BACKTRACKING_C1,
+                 rate=typedef.DEFAULT_BACKTRACKING_RATE,
+                 max_iter=typedef.DEFAULT_BACKTRACKING_MAX_ITER,
+                 alpha0=typedef.DEFAULT_BACKTRACKING_ALPHA0):
         if self.prev_x is None or self.prev_gk is None:
             self.prev_x, self.prev_gk = x, gk
             return backtracking(fun, x, gk, c1, rate, max_iter, alpha0)
@@ -44,4 +72,3 @@ class secant_symmetric(secant):
         ck = np.real(np.vdot(sk, yk))
         if ck > 0: return ck / (np.vdot(yk, yk) + np.finfo(float).eps)
         return backtracking(fun, x, gk, c1, rate, max_iter, alpha0)
-        # return np.abs(ck / (np.vdot(yk, yk) + np.finfo(float).eps))
