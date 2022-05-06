b05338479f784c18b1bb3790e2204653b549a764
Maksym Shpakovych <maksym.shpakovych@gmail.com>
2022-05-06 13:08:52 +0200

    Tests for methods were refactored.

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -24,7 +24,6 @@ class TestMethods(unittest.TestCase):
             x0=x0,
             tol=1e-6,
             max_iter=100,
-            seed=self.seed,
         )
         dist = phrt_opt.metrics.quality_norm(x, x_bar)
         self.assertAlmostEqual(dist, 0.00024642286613196784)
@@ -36,55 +35,18 @@ class TestMethods(unittest.TestCase):
             x0=x0,
             tol=1e-6,
             max_iter=100,
-            seed=self.seed,
         )
         dist = phrt_opt.metrics.quality_norm(x, x_bar)
         self.assertAlmostEqual(dist, 3.881517329773487e-09)
 
-    def test_phare_admm(self):
+    def test_admm(self):
         x, x0 = self.x, self.x0
-        x_bar = phrt_opt.methods.phare_admm(
+        x_bar = phrt_opt.methods.admm(
             self.tm, self.b,
             x0=x0,
             tol=1e-6,
             max_iter=100,
-            seed=self.seed,
-        )
-        dist = phrt_opt.metrics.quality_norm(x, x_bar)
-        self.assertAlmostEqual(dist, 2.682048674373938e-05)
-
-    def test_dual_ascent(self):
-        x, x0 = self.x, self.x0
-        x_bar = phrt_opt.methods.dual_ascent(
-            self.tm, self.b,
-            x0=x0,
-            tol=1e-6,
-            max_iter=100,
-            seed=self.seed,
-        )
-        dist = phrt_opt.metrics.quality_norm(x, x_bar)
-        self.assertAlmostEqual(dist, 0.0004532004668830192)
-
-    def test_relaxed_dual_ascent(self):
-        x, x0 = self.x, self.x0
-        x_bar = phrt_opt.methods.relaxed_dual_ascent(
-            self.tm, self.b,
-            x0=x0,
-            tol=1e-6,
-            max_iter=100,
-            seed=self.seed,
         )
         dist = phrt_opt.metrics.quality_norm(x, x_bar)
         self.assertAlmostEqual(dist, 2.2636871992776086e-05)
 
-    def test_accelerated_relaxed_dual_ascent(self):
-        x, x0 = self.x, self.x0
-        x_bar = phrt_opt.methods.accelerated_relaxed_dual_ascent(
-            self.tm, self.b,
-            x0=x0,
-            tol=1e-6,
-            max_iter=100,
-            seed=self.seed,
-        )
-        dist = phrt_opt.metrics.quality_norm(x, x_bar)
-        self.assertAlmostEqual(dist, 0.00143715897585861)
