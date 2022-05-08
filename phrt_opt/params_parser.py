cf59f5d864e2d2d93cde8184c5a50d80e940e985
Maksym Shpakovych <maksym.shpakovych@gmail.com>
2022-05-08 13:33:18 +0200

    Params parser for quadprogs was implemented.

--- phrt_opt/params_parser.py
+++ phrt_opt/params_parser.py
@@ -1,11 +1,16 @@
 from copy import deepcopy
+from phrt_opt.quadprog import get as get_quadprog
 from phrt_opt.linesearch import get as get_linesearch
 
 
 def linesearch(params: dict):
-    callable_obj = get_linesearch(params["name"])
+    linesearch_class = get_linesearch(params["name"])
     if "linesearch" not in params:
-        return callable_obj(**params["params"])
+        return linesearch_class(**params["params"])
     params_c = deepcopy(params["params"])
     nested = params_c.pop("linesearch")
-    return callable_obj(linesearch=linesearch(nested), **params_c)
+    return linesearch_class(linesearch=linesearch(nested), **params_c)
+
+
+def quadprog(params: dict):
+    return get_quadprog(params["name"])(**params["params"])
