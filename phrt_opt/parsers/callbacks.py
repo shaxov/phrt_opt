2a3d30aa0887ef639d4509813dc6e1140394f081
Maksym Shpakovych <maksym.shpakovych@gmail.com>
2022-05-08 18:16:20 +0200

    Callbacks parsers were refactored.

--- phrt_opt/parsers/callbacks.py
+++ phrt_opt/parsers/callbacks.py
@@ -1,21 +1,48 @@
+import numpy as np
+import phrt_opt.utils
 from phrt_opt.callbacks import counters
 
 
-def gradient_descent(tm, b, params: dict):
-    linesearch = params["linesearch"]
-    linesearch_params = linesearch["params"]
-    gradient_descent_callback = counters.GradientDescentCallback
-
-    if linesearch["name"] == counters.BacktrackingCallback.name():
-        return gradient_descent_callback(
-            counters.BacktrackingCallback(tm, b, linesearch_params),
-        )
-    if linesearch["name"] == counters.SecantCallback.name():
-        return gradient_descent_callback(
-            counters.SecantCallback(
+def _linesearch(tm, b, params: dict, preliminary_step: callable = None):
+    if params["name"] == counters.BacktrackingCallback.name():
+        return counters.BacktrackingCallback(
+            tm, b, params["params"], preliminary_step=preliminary_step)
+    if params["name"] == counters.SecantCallback.name():
+        linesearch_params = params["params"]
+        return counters.SecantCallback(
                 counters.BacktrackingCallback(
-                    tm, b, linesearch_params["linesearch"]["params"]),
+                    tm, b, linesearch_params["linesearch"]["params"]
+                ),
                 linesearch_params,
+                preliminary_step=preliminary_step,
             )
-        )
-    raise ValueError(f"Linesearch with name '{linesearch['name']}' is not valid.")
+    raise ValueError(f"Linesearch with name '{params['name']}' is not valid.")
+
+
+def _quadprog(tm, b, params: dict, preliminary_step: callable = None):
+    if params["name"] == counters.ConjugateGradientCallback.name():
+        return counters.ConjugateGradientCallback(
+            tm, b, params["params"], preliminary_step=preliminary_step)
+    if params["name"] == counters.CholeskyCallback.name():
+        return counters.CholeskyCallback(
+            tm, b, params["params"], preliminary_step=preliminary_step)
+    raise ValueError(f"Quadprog with name '{params['name']}' is not valid.")
+
+
+def gradient_descent(tm, b, params: dict):
+    return counters.GradientDescentCallback(_linesearch(tm, b, params["linesearch"]))
+
+
+def gauss_newton(tm, b, params: dict):
+    return counters.GaussNewtonCallback(
+        _linesearch(tm, b, params["linesearch"]),
+        _quadprog(tm, b, params["quadprog"],
+                  preliminary_step=phrt_opt.utils.define_gauss_newton_system(tm, b))
+    )
+
+def alternating_projections(tm, b, params: dict):
+    return counters.AlternatingProjectionsCallback(np.shape(tm))
+
+
+def admm(tm, b, params: dict):
+    return counters.ADMMCallback(np.shape(tm))
