45b7d945164c150b43b231c7cabe838100ea90c6
Maksym Shpakovych <maksym.shpakovych@gmail.com>
2022-05-08 14:46:07 +0200

    Renaming

--- phrt_opt/__init__.py
+++ phrt_opt/__init__.py
@@ -2,7 +2,7 @@ from . import ops
 from . import methods
 from . import metrics
 from . import callbacks
-from . import strategy
+from . import strategies
 from . import linesearch
 from . import initializers
 from collections import namedtuple
