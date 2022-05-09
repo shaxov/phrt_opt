e68fd7338dac55e8d8f023b4dec4ee72574aa989
Maksym Shpakovych <maksym.shpakovych@gmail.com>
2022-05-09 14:05:26 +0200

    Initializers were refactored.

--- phrt_opt/initializers.py
+++ phrt_opt/initializers.py
@@ -1,6 +1,5 @@
 import numpy as np
-from phrt_opt import utils
-from phrt_opt import typedef
+import phrt_opt.utils
 
 
 class _Initializer:
@@ -16,29 +15,26 @@ class Random(_Initializer):
     def __call__(self, tm, b):
         """ Random starting point generation. """
         dim = np.shape(tm)[1]
-        x0 = utils.random_x0(dim, self.random_state)
+        x0 = phrt_opt.utils.random_x0(dim, self.random_state)
         return x0
 
 
 class Wirtinger(_Initializer):
-
-    def __init__(self, tol=typedef.DEFAULT_POWER_METHOD_TOL, random_state=None):
-        super().__init__(random_state=random_state)
-        self.tol = tol
-
-    def __call__(self, tm, b):
-        """ Starting point computation via Wirtinger flow [1].
+    """ Starting point computation via Wirtinger flow [1].
 
         Reference:
             [1] Candes, Emmanuel & Soltanolkotabi, Mahdi. (2014). Phase Retrieval via Wirtinger Flow: Theory and Algorithms.
-             IEEE Transactions on Information Theory. 61. 10.1109/TIT.2015.2399924.
-        """
-        m, n = np.shape(tm)
-        b2 = np.square(b[..., np.newaxis])
-        mat = tm[..., np.newaxis].conj() * tm[:, np.newaxis]
-        mat = np.sum(b2 * mat, axis=0) / m
+            IEEE Transactions on Information Theory. 61. 10.1109/TIT.2015.2399924.
+    """
 
-        _, v = utils.power_method(mat, self.tol)
+    def __init__(self, eig: callable, random_state=None):
+        super().__init__(random_state=random_state)
+        self.eig = eig
+
+    def __call__(self, tm, b):
+        _, n = np.shape(tm)
+        matrix = phrt_opt.utils.compute_initialization_matrix(tm, b)
+        _, v = self.eig(matrix)
         lmd = np.linalg.norm(tm, axis=1)
         lmd = np.square(lmd)
         lmd = np.sqrt(n * np.sum(b) / np.sum(lmd))
